38db89f3851afdd917e985ace5b64286882b3e17
Matthew Wiewel <mjvwiewel@gmail.com>
2020-02-26 17:33:12 -0600

    added currently non-functional test

--- Coding/helpers_test.go
+++ Coding/helpers_test.go
@@ -1,3 +1,13 @@
 package coding
 
-//import "testing"
+import "testing"
+
+func TestTypeMapMakeB(t *testing.T) {
+	AutofillTypeMap()
+	var result AutoStruct = typeFactory(bID)
+	var bStruct B
+	var expected AutoStruct = &bStruct
+	if result != expected {
+		t.Errorf("Creating a struct of type B does not work")
+	}
+}
