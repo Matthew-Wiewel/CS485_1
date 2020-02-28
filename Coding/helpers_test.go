bb4b70cfd0c3e34e38c8fdd4bcf54c522231be65
Matthew Wiewel <mjvwiewel@gmail.com>
2020-02-28 08:50:25 -0600

    added tests for encoding and decoding of TypeId

--- Coding/helpers_test.go
+++ Coding/helpers_test.go
@@ -41,3 +41,83 @@ func TestByteEncoding_4(t *testing.T) {
 		t.Errorf("Encoded byte %b was decoded as %b", int(byteToEncode), int(decodedByte))
 	}
 }
+
+func TestUint64Encoding_1(t *testing.T) {
+	var uint64ToEncode uint64 = 0
+	encoding := encodeUint64(uint64ToEncode)
+	decoded := decodeUint64(encoding)
+
+	if uint64ToEncode != decoded {
+		t.Errorf("Encoded uint64 %d was decoded as %d", uint64ToEncode, decoded)
+	}
+}
+
+func TestUint64Encoding_2(t *testing.T) {
+	var uint64ToEncode uint64 = 0xFFFFFFFFFFFFFFFF
+	encoding := encodeUint64(uint64ToEncode)
+	decoded := decodeUint64(encoding)
+
+	if uint64ToEncode != decoded {
+		t.Errorf("Encoded uint64 %d was decoded as %d", uint64ToEncode, decoded)
+	}
+}
+
+func TestUint64Encoding_3(t *testing.T) {
+	var uint64ToEncode uint64 = 98468
+	encoding := encodeUint64(uint64ToEncode)
+	decoded := decodeUint64(encoding)
+
+	if uint64ToEncode != decoded {
+		t.Errorf("Encoded uint64 %d was decoded as %d", uint64ToEncode, decoded)
+	}
+}
+
+func TestUint64Encoding_4(t *testing.T) {
+	var uint64ToEncode uint64 = 36375
+	encoding := encodeUint64(uint64ToEncode)
+	decoded := decodeUint64(encoding)
+
+	if uint64ToEncode != decoded {
+		t.Errorf("Encoded uint64 %d was decoded as %d", uint64ToEncode, decoded)
+	}
+}
+
+func TestEncodeTypeId_1(t *testing.T) {
+	encode := bID
+	encoding := encodeTypeId(encode)
+	decode := decodeTypeId(encoding)
+
+	if encode != decode {
+		t.Errorf("TypeId %d was decoded as %d", encode, decode)
+	}
+}
+
+func TestEncodeTypeId_2(t *testing.T) {
+	encode := cID
+	encoding := encodeTypeId(encode)
+	decode := decodeTypeId(encoding)
+
+	if encode != decode {
+		t.Errorf("TypeId %d was decoded as %d", encode, decode)
+	}
+}
+
+func TestEncodeTypeId_3(t *testing.T) {
+	encode := dID
+	encoding := encodeTypeId(encode)
+	decode := decodeTypeId(encoding)
+
+	if encode != decode {
+		t.Errorf("TypeId %d was decoded as %d", encode, decode)
+	}
+}
+
+func TestEncodeTypeId_4(t *testing.T) {
+	encode := eID
+	encoding := encodeTypeId(encode)
+	decode := decodeTypeId(encoding)
+
+	if encode != decode {
+		t.Errorf("TypeId %d was decoded as %d", encode, decode)
+	}
+}
