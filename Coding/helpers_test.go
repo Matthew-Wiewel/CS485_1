67601b8119a5ca27d7c09c29d6c290e562561b45
Matthew Wiewel <mjvwiewel@gmail.com>
2020-02-28 08:42:01 -0600

    added encoding and decoding byte tests

--- Coding/helpers_test.go
+++ Coding/helpers_test.go
@@ -2,12 +2,42 @@ package coding
 
 import "testing"
 
-func TestTypeMapMakeB(t *testing.T) {
-	AutofillTypeMap()
-	var result AutoStruct = typeFactory(bID)
-	var bStruct B
-	var expected AutoStruct = &bStruct
-	if result != expected {
-		t.Errorf("Creating a struct of type B does not work")
+func TestByteEncoding_1(t *testing.T) {
+	var byteToEncode byte = 0
+	encoding := encodeByte(byteToEncode)
+	decodedByte := decodeByte(encoding)
+
+	if byteToEncode != decodedByte {
+		t.Errorf("Encoded byte %b was decoded as %b", int(byteToEncode), int(decodedByte))
+	}
+}
+
+func TestByteEncoding_2(t *testing.T) {
+	var byteToEncode byte = 127
+	encoding := encodeByte(byteToEncode)
+	decodedByte := decodeByte(encoding)
+
+	if byteToEncode != decodedByte {
+		t.Errorf("Encoded byte %b was decoded as %b", int(byteToEncode), int(decodedByte))
+	}
+}
+
+func TestByteEncoding_3(t *testing.T) {
+	var byteToEncode byte = 75
+	encoding := encodeByte(byteToEncode)
+	decodedByte := decodeByte(encoding)
+
+	if byteToEncode != decodedByte {
+		t.Errorf("Encoded byte %b was decoded as %b", int(byteToEncode), int(decodedByte))
+	}
+}
+
+func TestByteEncoding_4(t *testing.T) {
+	var byteToEncode byte = 92
+	encoding := encodeByte(byteToEncode)
+	decodedByte := decodeByte(encoding)
+
+	if byteToEncode != decodedByte {
+		t.Errorf("Encoded byte %b was decoded as %b", int(byteToEncode), int(decodedByte))
 	}
 }
